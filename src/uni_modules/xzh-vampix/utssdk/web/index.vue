e702aca3ac38fe8b6fc0275c1298a0c0fd010c83
xzh <10000000@qq.com>
2023-05-12 20:15:13 +0800

    feat: web插件

---
+++ src/uni_modules/xzh-vampix/utssdk/web/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view :style="grayscale">
+        <slot></slot>
+	</view>
+</template>
+
+<script lang="uts">
+	export default {
+		name: "xzh-vampix",
+		props: {
+			sat: {
+				type: Number,
+				default: 1
+			},
+		},
+		computed: {
+			grayscale() {
+				return { filter: `grayscale(${this.sat * 100}%)` }
+			}
+		}
+	}
+</script>
